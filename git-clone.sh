e2a982f85b7dd050f7e809f716c7e814f366011b
ibrahim00017 <hibrahimyya@gmail.com>
2018-07-30 08:47:39 +0100

    media address

--- git-clone.sh
+++ git-clone.sh
@@ -6,41 +6,81 @@
 #branch=$2
 #user=$3
 #password=$4
+branch_jars=release_1.4
+branch_sf=release_2.0
 pag=echo "pwd"
 echo "$pag"
 git clone https://github.com/ibrahim00017/scripts.git 
 
-mkdir jars && cd jars
+mkdir jars 
+cd jars
 jars=echo "pwd"
+projets_sf=("categorie.service.fonctionnel" "projet.service.fonctionnel" "commentaire.service.fonctionnel" "media.service.fonctionnel" "liketable.service.fonctionnel" "note.service.fonctionnel" "debat.service.fonctionnel" "censure.service.fonctionnel" "sondage.service.fonctionnel" "notification.generator.services" "notification.diffusion.services" "preference.service.fonctionnel" "programme.service.fonctionnel" "debat.service.use.case" "bootcamp.user" "statistique.service.fonctionnel")
 
-echo "============CLONAGE DES RESOURCES JARS====================="
-./../scripts/git-clone-jar.sh "$jars" release_1.4.0
+projet_jar=("bootcamp.common" "bootcamp.database" "service.crud" "bootcamp.commonws" "bootcamp.rest.service.client" "bootcamp.security" "notification.diffusion.services")
+
+clone_project_jar () {
+    for projet in ${projet_jar[@]}
+        do
+            # $1 paramet  er is the name of the project to clone
+            cd "$jars"
+            echo "------------------------------ clone  $projet_jar  project----------------------------------------"
+            git clone https://github.com/rintiobootcamp/"$projet_jar".git -b "$branch_jars"
+    done
+}
+
 
 git clone https://github.com/ibrahim00017/pagdevops.git
 cd pagdevops
 pagdevops=echo "pwd"
-cd ..
-echo "=========================CLONAGE DES SERVICES DU PAG=================="
-./scripts/git-clone-sf.sh release_2.0
-echo "=========================FIN CLONAGE DES SERVICES DU PAG============================="
 
-echo "=========================CLONAGE DE LA RESSOURCE elasticsearch============================"
-git clone -b dev https://github.com/ibrahim00017/elasticsearch.service.git
+clone_project_sf () {
+    for projet in ${projets_sf[@]}
+        do
+            # $1 paramet  er is the name of the project to clone
+            cd "$pagdevops"
+            echo "------------------------------ clone  $projets_sf  project----------------------------------------"
+            git clone https://github.com/rintiobootcamp/"$projets_sf".git -b "$branch_sf"
+    done
+}
 
-echo "===========COPIE DES DOSSIER CONFIG MEDIA EPUB DANS LE HOME=============="
-cp -r scripts/home /home
 
-echo "=======FIN DE LACOPIE DES DOSSIER CONFIG MEDIA EPUB DANS LE HOME========"
+echo "========CLONAGE DES RESOURCES JARS=============="
+clone_project_jar
+echo "========FIN CLONAGE DES RESOURCES JARS=========="
 
-echo "=======DEPLOYEMENT DES RESSOURCES JARS========"
-cp scripts/jar/pom.xml "$jar"
 
-cd jars && mvn -DskipTests install deploy 
-cd ../elasticsearch.service
-mvn -DskipTests install deploy
 
-cd "$pagdevops"
-mvn -DskipTests  -P dev clean install
+echo "========CLONAGE DES RESOURCES JARS=============="
+clone_project_sf
+echo "========FIN CLONAGE DES RESOURCES JARS=========="
+
+
+# git clone https://github.com/ibrahim00017/pagdevops.git
+# cd pagdevops
+# pagdevops=echo "pwd"
+# cd ..
+# echo "========CLONAGE DES SERVICES DU PAG=================="
+# ./scripts/git-clone-sf.sh release_2.0
+# echo "==============FIN CLONAGE DES SERVICES DU PAG==================="
+
+# echo "=========CLONAGE DE LA RESSOURCE elasticsearch=========="
+# git clone -b dev https://github.com/ibrahim00017/elasticsearch.service.git
+
+# echo "===========COPIE DES DOSSIER CONFIG MEDIA EPUB DANS LE HOME=============="
+# cp -r scripts/home /home
+
+# echo "=======FIN DE LACOPIE DES DOSSIER CONFIG MEDIA EPUB DANS LE HOME========"
+
+# echo "=======DEPLOYEMENT DES RESSOURCES JARS========"
+# cp scripts/jar/pom.xml "$jar"
+
+# cd jars && mvn -DskipTests install deploy 
+# cd ../elasticsearch.service
+# mvn -DskipTests install deploy
+
+# cd "$pagdevops"
+# mvn -DskipTests  -P dev clean install
 
 
 
